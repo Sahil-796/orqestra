58574666008e90428262dbc7380905a3dcbd79c6
Sahil-796 <sp0709006@gmail.com>
2026-03-14 23:59:19 +0530

    refactor: raname stepcontext to workflowcontext because it stores context of entire workflow

--- src/types/index.ts
+++ src/types/index.ts
@@ -31,10 +31,10 @@ export type StepState = {
 }
 
 
-export type StepContext = {
+export type WorkflowContext = {
   workflowId: string
   payload: unknown //  ctx passed to the whole workflow
   step: <T>(name: string, fn: () => Promise<T>) => Promise<T> // step is a func which receves name, fn and returns T
 }
 
-export type WorkflowHandler = (ctx: StepContext) => Promise<void>
+export type WorkflowHandler = (ctx: WorkflowContext) => Promise<void>
